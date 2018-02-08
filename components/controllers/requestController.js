95513ae1f4e91bfa660689be67c4e28c9d3bc9bd
Marcio Henrique <marcio.lima@mangue3.com>
2018-02-08 14:39:53 -0300

    Upload File

--- components/controllers/requestController.js
+++ components/controllers/requestController.js
@@ -7,46 +7,40 @@ const mongoose = require('mongoose'),
 
 function upload(req, res) {
     let id_user = req.url.split('?')[1].split('=')[1];
-    let file = req.file.buffer.toString(); 
+    let file = req.file.buffer.toString();
 
     //console.log(file);
-    let newRequest = null;
+    let data = [];
 
-    csv({flatKeys:true})
-    .fromString(file)
-    .on('json',(jsonObj)=>{
-        console.log(jsonObj);
-        // newRequest = new Request({
-        //     'id_user': id_user,
-        //     'json_file': {'data': [jsonObj]},
-        //     'status': false
-        // });
+    csv({ flatKeys: true })
+        .fromString(file)
+        .on('json', (jsonObj) => {
+            data.push(jsonObj);
+        })
+        .on('done', (error) => {
+            console.log(error);
+        })
 
-        // newRequest.save();
-        
-        // res.json({
-        //     'status': 'sucess',
-        //     'json_file_id': newRequest._id
-        // });
-       
-    })
-    .on('done',(error)=>{
-        console.log(error);
-        res.json({
-            'status': 'error',
-            'json_file_id': {}
-        });
-    })
-     
-    
-    
+    let newRequest = new Request({
+        'id_user': id_user,
+        'json_file': { 'data': data },
+        'status': false
+    });
+
+    newRequest.save();
+
+    res.json({
+        'status': 'sucess',
+        'json_file_id': newRequest._id
+    });
+
 
     //let src = fs.createReadStream(tmp_path);
     //let dest = fs.createWriteStream(target_path);
 
     // src.pipe(dest);
     // src.on('end', function () {
-        
+
     // });
     // src.on('error', function (err) {
     //     res.send('error');
